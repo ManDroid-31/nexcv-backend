785722e1afa811ca015f3f0e867a062fded82ca3
Manas_on_this_terminal <manasshinde2005@gmail.com>
2025-06-11 18:44:06 +0530

    lint error resolve in config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended", "prettier", "eslint:recommended", "plugin:prettier/recommended", "plugin:node/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended", "prettier"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      }
+    }
+  }
 ]);
